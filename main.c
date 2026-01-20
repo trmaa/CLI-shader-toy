59f389ae565120dadf4136dfcfacf019fdb81287
trmaa <trmaayt@gmail.com>
2026-01-20 19:26:11 +0100

    cleanness

--- main.c
+++ main.c
@@ -3,24 +3,25 @@
 #include <unistd.h>
 #include "screen.c"
 
+void
+shader(struct screen *scr, float time, int i, int j)
+{
+
+	float viewport = (float)scr->w / scr->h;
+
+	float u = ((float)i / (scr->w-1))*2 - 1;
+	float v = ((float)j / (scr->h-1))*2 - 1;
+
+	u *= viewport;
+
+	float rad = (u*u + v*v) / (1 + viewport*viewport); // from 0 to 1;
+
+	int col = 0, r = 0, g = 0, b = 0;
+
 /*
  * This is the only part of the code that you should touch to use the tool.
  * Modify the shader to your will to render any image you like!
  */
-void
-shader(struct screen *scr, float time, int i, int j)
-{
-	float u = ((float)i / (scr->w-1))*2 - 1;
-	float v = ((float)j / (scr->h-1))*2 - 1;
-
-	u *= (float)scr->w / scr->h;
-
-	float rad = sqrtf(u*u + v*v);
-	if (rad > 1)
-		rad = 1;
-
-	int col = 0, r, g;
-
 	r = 255 * (((float)i / (scr->w-1))*2 - 1);
 	g = 255 * (((float)j / (scr->h-1))*2 - 1);
 
@@ -32,11 +33,19 @@ shader(struct screen *scr, float time, int i, int j)
 	if (g < 0)
 		g *= -1;
 
+	if (rad < 0.05) {
+		r = 255;
+		g = 255;
+		b = 255;
+	}
+
+/*
+ * The end.
+ */
+
+	col = b;
 	col += g << 8;
 	col += r << 16;
-
-	if (rad < 0.5)
-		col = 0xffffff;
 
 	scr->set_px(&scr->px[i + j * scr->w], col);
 }
